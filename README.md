79b7abfe7f5554e4f6a31d0dcfed4984a9c02759
swufez <123456@kk.com>
2024-10-27 21:37:54 +0800

    MyRPC Version2版本的实现

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ pratice to implement rpc by Java
 1. 只能调用服务端Service唯一确定的方法，如果有两个方法需要调用呢？(Request需要抽象)
 2. 返回值只支持User对象，如果需要传一个字符串或者Dog类，String对象呢(Response需要抽象)
 3. 客户端不够通用，host，port，与调用的方法都特定(需要抽象)
+
+## MyRPCVersion1
+common新增blog实体
+service新增BlogService和BlogServiceImpl以及ServiceProvider
+server整体抽象重构
+client没变，只是RPCclient多了一行调用代码
---
+++ myRPCVersion2/src/main/java/com/rpc/server/SimpleRPCServer.java
@@ -0,0 +1,42 @@
+package com.rpc.server;
+
+import com.rpc.service.ServiceProvider;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+
+/**
+ * 这个实现类代表着java原始的BIO监听模式，来一个任务，就new一个线程去处理
+ * 处理任务的工作见WorkThread中
+ */
+public class SimpleRPCServer implements RPCServer{
+    // 存着服务接口名-> service对象的map
+//    private Map<String, Object> serviceProvide;
+    private ServiceProvider serviceProvide;
+    public SimpleRPCServer(ServiceProvider serviceProvide){
+        this.serviceProvide = serviceProvide;
+    }
+    @Override
+    public void start(int port) {
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("服务端启动了");
+            // BIO的方式监听Socket
+            while (true){
+                Socket socket = serverSocket.accept();
+                // 开启一个新线程去处理
+                new Thread(new WorkThread(socket,serviceProvide)).start();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("服务器启动失败");
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
